162cc564447f2dc73d218d9e4d01d69d23daf808
Maurdekye <ncolaprete@gmail.com>
2024-01-22 23:03:18 -0500

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{stdin, stdout, Write},
+    path::PathBuf,
+};
+
+use thiserror::Error;
+
+use clap::Parser;
+
+mod lexer;
+
+#[derive(Clone, Debug, Error)]
+enum Error {
+    
+}
+
+fn run(source: String) -> Result<(), Error> {
+    println!("'{source}'");
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Args {
+    file: Option<PathBuf>,
+
+    #[clap(short, long)]
+    source: Option<String>,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    match (args.file, args.source) {
+        (_, Some(source)) => run(source)?,
+        (Some(file), _) => {
+            let source = std::fs::read_to_string(file)?;
+            run(source)?;
+        }
+        _ => loop {
+            print!("> ");
+            stdout().flush()?;
+            let Some(maybe_line) = stdin().lines().next() else {
+                break;
+            };
+            let line = maybe_line?;
+            run(line)?;
+        },
+    }
+
+    Ok(())
 }
